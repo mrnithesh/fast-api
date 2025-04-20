e3a163cd4e0357e0f6d6120c0435c9f0cc9fbf09
mrnithesh <mr.nithesh.k@gmail.com>
2025-04-20 13:24:40 +0530

    practiced post methods with pydantic validation

--- basics.py
+++ basics.py
@@ -1,4 +1,10 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class User(BaseModel):
+    name : str
+    email: str
+    age : int
 
 app= FastAPI()
 
@@ -16,6 +22,15 @@ def greet_with_age(name : str = "user", age : int = 69):
             "age": age
         }
 
+
+@app.post("/create-user")
+def create_user(user: User):
+    return {
+        "message": f"Welcome {user.name}!",
+        "email": user.email,
+        "age": user.age
+    }
+
 if __name__=="__main__":
     import uvicorn
-    uvicorn.run("basics:app", host="127.0.0.1", port=8001, reload=True)
+    uvicorn.run("basics:app", host="127.0.0.1", port=8000, reload=True)
